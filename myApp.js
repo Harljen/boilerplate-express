6344019a7717b8ce2982143721183c9c3ab0e269
harljenhill <hillharljen@gmail.com>
2021-08-23 21:13:16 -0400

    changing output of the page

--- myApp.js
+++ myApp.js
@@ -1,44 +1,24 @@
 let express = require('express');
 let app = express();
 
-app.get(function (req,res){
-        res.send("Hello Express");
+// app.get("/",function (req,res){
+//         // Normal usage
+//         //app.use(express.static(__dirname + "/public"));
+//
+//         // Assets at the /public route
+//         app.use("/public", express.static(__dirname + "/public"));
+//     res.sendFile(__dirname + "/views/index.html");
+//     }
+// );
+app.get("/json", (req, res) => {
+
+    let response = "Hello json";
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+        response = response.toUpperCase();
     }
-);
+    res.json({
+        message: process.env.MESSAGE_STYLE
+    });
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
